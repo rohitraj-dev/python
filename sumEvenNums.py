ae5e77157c73ee50b284b21ed01911875a25ce66
Rohit Raj <rajrohit7388@outlook.com>
2026-06-13 22:58:23 +0530

    Add files via upload

---
+++ sumEvenNums.py
@@ -0,0 +1,10 @@
+numbers = [1, -2, 3, -4, 5, 6, -7, -8, 9, 10]
+
+sum_even = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        sum_even += num
+        
+        
+print("Sum of even numbers : ", sum_even)
